d326a94b70e61ac8694441ab3b7cd6bc3a9d3318
Jose <romerojosemanuel@hotmail.com>
2024-07-20 17:21:13 -0600

    add command: helpMeWithTailwindCss

--- src/extension.ts
+++ src/extension.ts
@@ -7,10 +7,16 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('easy-tailwindcss.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Easy TailwindCss!');
+	const disposable = vscode.commands.registerCommand('easy-tailwindcss.helpMeWithTailwindCss', async () => {
+		
+		let message: string | undefined = await vscode.window.showInputBox({
+			prompt: "Enter your message",
+		});
+
+		if (message) {
+			vscode.window.showInformationMessage(message);
+		}
+
 	});
 
 	context.subscriptions.push(disposable);
