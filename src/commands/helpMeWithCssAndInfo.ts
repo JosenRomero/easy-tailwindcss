6398fe94bb648b648a07641fdf44c3030b1656b9
Jose <romerojosemanuel@hotmail.com>
2024-08-01 16:23:42 -0600

    update the helpMeWithCssAndInfo file

--- src/commands/helpMeWithCssAndInfo.ts
+++ src/commands/helpMeWithCssAndInfo.ts
@@ -10,6 +10,11 @@ const helpMeWithCssAndInfo = async (text: string) => {
     return;
   }
 
+  if (!text) {
+    vscode.window.showWarningMessage("If you want to get a TailwindCSS utility class, you can use the command 'Help me with TailwindCSS'.");
+    return;
+  }
+
   const res = await aiMessage(text);
  
   if (res) {
