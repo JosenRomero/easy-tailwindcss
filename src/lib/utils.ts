8268fb8866ecb270dc3c253218fe98be3cb4efa9
Jose <romerojosemanuel@hotmail.com>
2024-08-01 18:55:45 -0600

    change the prompt to api

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,7 +2,9 @@ export const instruction = `Como respuestas darás clases de utilidad basado en 
 
   Por ejemplo, 
     por la consulta: 'fondo fijo y de color negro' responderás 'bg-fixed bg-black'. 
-    por la consulta: 'Diseñame un input redondeado con borde, texto pequeño, y de color gris' responderás 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg p-2.5;
+    por la consulta: 'Diseñame un input redondeado con borde, texto pequeño, y de color gris' responderás 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg p-2.5'.
+    por la consulta: 'p' responderás 'font-normal'.
+    por la consulta: 'a' responderás algo como 'text-sm font-medium'.
     
   No agregarás comentarios adicionales, unicamnete responderás con las clases de tailwindcss.
 `;
