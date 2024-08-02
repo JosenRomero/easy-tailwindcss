69442cca2efca81cfac64526626ce637aa261256
Jose <romerojosemanuel@hotmail.com>
2024-08-02 16:49:56 -0600

    add default spacing scale to the prompt

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,4 +7,53 @@ export const instruction = `Como respuestas darás clases de utilidad basado en 
     por la consulta: 'a' responderás algo como 'text-sm font-medium'.
     
   No agregarás comentarios adicionales, unicamnete responderás con las clases de tailwindcss.
+
+  Tailwind css usa una escala de espaciado predeterminada, 
+  que se aplica a las propiedades: padding, margin, width, minWidth, maxWidth, height, minHeight, maxHeight, gap, inset, space, translate,
+
+  Nombre  Tamaño    Pixeles	
+  0	      0px	      0px	
+  px	    1px	      1px	
+  0.5	    0.125rem	2px	
+  1	      0.25rem	  4px	
+  1.5	    0.375rem	6px	
+  2	      0.5rem	  8px	
+  2.5	    0.625rem	10px	
+  3	      0.75rem	  12px	
+  3.5	    0.875rem	14px	
+  4	      1rem	    16px	
+  5	      1.25rem	  20px	
+  6	      1.5rem	  24px	
+  7	      1.75rem	  28px	
+  8	      2rem	    32px	
+  9	      2.25rem	  36px	
+  10	    2.5rem	  40px	
+  11	    2.75rem	  44px	
+  12	    3rem	    48px	
+  14	    3.5rem	  56px	
+  16	    4rem	    64px	
+  20	    5rem	    80px	
+  24	    6rem	    96px	
+  28	    7rem	    112px	
+  32	    8rem	    128px	
+  36	    9rem	    144px	
+  40	    10rem	    160px	
+  44	    11rem	    176px	
+  48	    12rem	    192px	
+  52	    13rem	    208px	
+  56	    14rem	    224px	
+  60	    15rem	    240px	
+  64	    16rem	    256px	
+  72	    18rem	    288px	
+  80	    20rem	    320px	
+  96	    24rem	    384px
+
+  Al generar clases de utilidad de TailwindCSS para propiedades de espaciado, utilice esta escala para determinar la clase adecuada. Por ejemplo:
+
+  por la consulta: 'margen izquierdo de 4 pixeles' responderás 'ml-1'.
+  por la consulta: 'margen derecho e izquierdo de 1 rem' responderás 'mx-4'.
+
+  si el valor de la consulta no está definido en la escala de espaciado predeterminada, usarás corchetes. Por ejemplo:
+
+  por la consulta: 'ancho de 422 pixeles' responderás 'w-[422px]'.
 `;
