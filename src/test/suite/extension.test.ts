6237de48b028247c867364aa4ba85a0043782cba
Jose <romerojosemanuel@hotmail.com>
2024-08-26 16:46:45 -0600

    add tests

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -8,8 +8,42 @@ import * as vscode from 'vscode';
 suite('Extension Test Suite', () => {
   vscode.window.showInformationMessage('Start all tests.');
 
+  suiteSetup( async () => {
+
+    const ext = vscode.extensions.getExtension('josenromero.easy-tailwindcss');
+    await ext?.activate();
+
+  });
+
   test('Sample test', () => {
     assert.strictEqual(-1, [1, 2, 3].indexOf(5));
     assert.strictEqual(-1, [1, 2, 3].indexOf(0));
   });
+
+  test('can activate the extension', async () => {
+    const ext = vscode.extensions.getExtension('josenromero.easy-tailwindcss');
+    const activate = ext?.isActive;
+    assert.strictEqual(activate, true);
+  });
+
+  test('commands are registered in vscode', async () => {
+
+    const registeredCommands = await vscode.commands.getCommands();
+  
+    const expectedCommands = [
+      "easy-tailwindcss.askAPIkey",
+      "easy-tailwindcss.removeAPIkey",
+      "easy-tailwindcss.helpMeWithTailwindCss"
+    ];
+
+    for (const expectedCommand of expectedCommands ) {
+      assert.notStrictEqual(
+        registeredCommands.indexOf(expectedCommand),
+        -1,
+        `command ${expectedCommand} not registered and was expected`
+      );
+    }
+
+  });
+
 });
